8071217628f9abd11ea9b68f0c632c4631ff1731
kasun beddewela <kbeddewela@aampglobal.com>
2025-02-15 17:55:49 -0500

    we modularized the code to make it more clean and readable

--- readGDock.py
+++ readGDock.py
@@ -19,14 +19,46 @@ long_doc = "https://docs.google.com/document/d/e/2PACX-1vQGUck9HIFCyezsrBSnmENk5
 def extract_data(input_string ):
     val_idx = input_string.find("</span>")
     col1_val = input_string[val_idx -1 : val_idx]
-    print("col val:" + col1_val )
+    #print("col val:" + col1_val )
     return col1_val
 
 
+def extract_data_from_row(row_string):
+    col_end_idx = row_string.find("</td>")
+    i = 0 
+    while col_end_idx != -1:
+        col_str = row_string[ : col_end_idx + len("</td>")]
+        row_string = row_string[col_end_idx + len("</td>") : ]
+        #print("col:" + col_str)
+        if i == 0:
+            x = extract_data(col_str)
+        elif  i == 1:
+            y = extract_data(col_str)
+        else :
+            z =extract_data(col_str)
+        i = i+1
+        extract_data(col_str)
+        col_end_idx = row_string.find("</td>")
+    return (x,y,z)        
 
 
-
-
+def extract_rows_from_table(table_string):
+    row_end_idx = table_string.find("</tr>")
+    data_tuples = []
+    while row_end_idx != -1:
+        #print("row index:" + str(row_end_idx))
+        #print(table_string)
+        row_str = table_string[: row_end_idx + len("</tr>") ]
+        table_string = table_string[row_end_idx  + len("</tr>") : ]
+        #print("row:" + row_str)
+        x, y, z = extract_data_from_row(row_str)
+        print("data:" + str(x) +":" + str(y) + ":" + str(z) )
+        data_tuples.append((x ,y, z)) #append the data to list
+        row_end_idx = table_string.find("</tr>")  
+    return data_tuples
+    
+    
+ 
 
 
 
@@ -47,7 +79,7 @@ and then removing it
 """
 header_end_idx = response_str.find("</tr>")
 response_str = response_str[header_end_idx + len("</tr>") : len(response_str) ]
-#print(response_str)
+print(response_str)
 """
 row_end_idx = response_str.find("</tr>")
 row_str = response_str[: row_end_idx + len("</tr>")  ]
@@ -55,12 +87,15 @@ print(row_str)
 response_str = response_str[row_end_idx  + len("</tr>"): ]
 print(response_str) """
 
+
+"""
 row_end_idx = response_str.find("</tr>")
 while row_end_idx != -1:
     row_str = response_str[: row_end_idx + len("</tr>") ]
     response_str = response_str[row_end_idx  + len("</tr>") : ]
     print("row:" + row_str)
-    col_end_idx = response_str.find("</td>")
+    #col_end_idx = response_str.find("</td>")
+    col_end_idx = row_str.find("</td>")
     while col_end_idx != -1:
         col_str = row_str[ : col_end_idx + len("</td>")]
         row_str = row_str[col_end_idx + len("</td>") : ]
@@ -69,6 +104,8 @@ while row_end_idx != -1:
         col_end_idx = row_str.find("</td>")
         
     row_end_idx = response_str.find("</tr>")  
+"""
+extract_rows_from_table(response_str)
     
     
 
